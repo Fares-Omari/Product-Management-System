b3b9086e5f4febc4d3ddd2076eb7222c0e26f9bc
Fares <2020880071@ses.yu.edu.jo>
2025-01-25 23:20:46 +0300

    solve the seed problem

--- vatrenas_project/db/seeds.rb
+++ vatrenas_project/db/seeds.rb
@@ -1,15 +1,15 @@
-require 'bcrypt'
+
 
 User.create!(
   name: "Fares Omari",
   email: "faresomarii13@gmail.com",
-  encrypted_password: BCrypt::Password.create("123456"),
+  password: "123456",
   role: "admin"
 )
   User.create!(
   name: "Mohammad",
   email: "mohammad@gmail.com",
-  encrypted_password: BCrypt::Password.create("123456"),
+  password: "123456",
   role: "user"
 )
 
