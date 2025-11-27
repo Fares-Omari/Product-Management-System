fa56d3d2c97bb93751749775166108c5a3005b67
Fares-Omari <fares.omari@vatrenas.com>
2025-11-27 23:48:30 +0300

    fix

--- vatrenas_project/config/environments/production.rb
+++ vatrenas_project/config/environments/production.rb
@@ -1,6 +1,8 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  config.assets.initialize_on_precompile = false
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
